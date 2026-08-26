e3dd544307b2ff2295f90f80eeee584e67f72405
renhaow89 <renhaow89@gmail.com>
2026-08-26 17:56:37 +0800

    fix: register journal and file panels in frontend entry

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,8 @@
 import { TaskPanel } from './task-panel.js';
 import './reading-panel.js';
 import './research-panel.js';
+import './journal-panel.js';
+import './file-panel.js';
 import './dashboard.js';
 
 window.TaskPanel = TaskPanel;
